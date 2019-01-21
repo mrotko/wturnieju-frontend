d01a8a4a25e0bb13fe2ed83f9bb4394574fcb2b6
Michał Rotko <michalr835@gmail.com>
2019-01-21 23:35:48 +0100

    fix

--- src/app/helper/jwt.interceptor.ts
+++ src/app/helper/jwt.interceptor.ts
@@ -1,13 +1,19 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/internal/Observable';
+import {AuthService} from '../service/auth.service';
 
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
+  constructor(
+    private authService: AuthService
+  ) {
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const currentUser = this.authService.getUserFromStorage();
     if (currentUser && currentUser.token) {
       req = req.clone({
         setHeaders: {
